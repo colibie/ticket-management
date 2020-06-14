a635a4396c0efffbe62aacff0b7edbaf21932156
Olibie Chidera <jenniferolibie@gmail.com>
2020-06-14 21:00:17 +0100

    updated sidenav items

---
+++ src/data/sidebar-nav-items.js
@@ -0,0 +1,49 @@
+export default function() {
+  return [
+    {
+      title: "Sell Tickets",
+      htmlBefore: '<i class="material-icons">view_module</i>',
+      to: "/sell-tickets",
+    },
+    {
+      title: "Movies",
+      htmlBefore: '<i class="material-icons">view_module</i>',
+      to: "/movies",
+    },
+    {
+      title: "Users",
+      htmlBefore: '<i class="material-icons">person</i>',
+      to: "/users",
+    },
+    {
+      title: "Create Admin",
+      htmlBefore: '<i class="material-icons">person</i>',
+      to: "/create-admin",
+    },
+    {
+      title: "Blog Posts",
+      htmlBefore: '<i class="material-icons">vertical_split</i>',
+      to: "/blog-posts",
+    },
+    {
+      title: "Add New Post",
+      htmlBefore: '<i class="material-icons">note_add</i>',
+      to: "/add-new-post",
+    },
+    {
+      title: "Forms & Components",
+      htmlBefore: '<i class="material-icons">view_module</i>',
+      to: "/components-overview",
+    },
+    {
+      title: "User Profile",
+      htmlBefore: '<i class="material-icons">person</i>',
+      to: "/user-profile-lite",
+    },
+    {
+      title: "Errors",
+      htmlBefore: '<i class="material-icons">error</i>',
+      to: "/errors",
+    }
+  ];
+}
